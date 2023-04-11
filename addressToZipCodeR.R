fd3e092ecbf326940ff6633b381d624345fab1ee
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2023-04-11 16:13:53 -0400

    address to zipcode R

---
+++ addressToZipCodeR.R
@@ -0,0 +1,51 @@
+rm(list=ls())
+gc()
+directory = "~/Documents/Masters/data712/"
+setwd(directory)
+
+library(httr)
+library(readr)
+library(xml2)
+
+# see link below for more details
+# https://www.usps.com/business/web-tools-apis/address-information-api.htm#_Toc110511817
+params = list(
+   # Delivery Address in the destination address. May contain secondary unit 
+   # designator, such as APT or SUITE, for Accountable mail.). Optional
+   address1 = ""
+  
+   # Delivery Address in the destination address. Required.
+  ,address2 = "65-30 Kissena Blvd"
+  
+  # City name of the destination address. Optional
+  ,city = "Queens"
+  
+  # Two-character state code of the destination address. Optional.
+  ,state = "NY"
+)
+
+# usps_key is the name of the file where my secret key is stored. Get your own.
+secret_key = read_file("usps_key")
+
+queryString <- list('xml'= paste0(
+    "<ZipCodeLookupRequest USERID=\""
+    ,secret_key
+    ,"\"><Address><Address2>"
+    ,params$address2
+    ,"</Address2><City>"
+    ,params$city
+    ,"</City><State>"
+    ,params$state
+    ,"NY</State></Address></ZipCodeLookupRequest>"
+  )
+  ,'API' = "ZipCodeLookup"
+)
+
+url <- "http://production.shippingapis.com/ShippingAPI.dll"
+
+encode <- "raw"
+
+response <- VERB("GET", url, query = queryString, content_type("application/xml"), encode = encode)
+
+# Get zipcode
+xml_text(xml_find_all(read_xml(response), ".//Zip5"))
